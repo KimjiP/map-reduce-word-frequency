d410d29c8226bff4e5061c29ec4211122d938b81
Kimji <kimji.pellano@gmail.com>
2021-04-19 19:12:11 +0200

    Add files via upload

---
+++ Mapper.java
@@ -0,0 +1,51 @@
+package com.mycompany.mpwordcount;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author kimji
+ */
+public class Mapper {
+    
+    public static String fileLocation = "C:\\Users\\kimji\\Desktop\\big data\\3rd assignment\\workshop.txt";
+    private ArrayList<String> a = new ArrayList<String>();
+    
+    public ArrayList<String> getList(){
+        return this.a;
+    }
+    
+    public void load(){
+        try{
+            FileReader f = new FileReader(fileLocation);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()){
+                String line = s.nextLine();
+                String[] words = line.split("\\s");
+                for (String word : words) {
+                    if (!word.isEmpty()){
+                        word = word.toLowerCase();
+                        word = word.replace(",","");
+                        word = word.replace(".","");
+                        word = word.replace(";","");
+                        word = word.replace("\"","");
+                        word = word.replace("?","");
+                        word = word.replace("(","");
+                        word = word.replace(")","");
+                        word = word.replace(":","");
+                        word = word.replace("'s","");
+                        a.add(word);
+                    }
+                }
+            }
+            f.close();
+        }catch (IOException ex1){
+            System.out.println("An error occured.");
+            ex1.printStackTrace();
+        }
+    }
+    
+}
---
+++ Reducer.java
@@ -0,0 +1,74 @@
+package com.mycompany.mpwordcount;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import static java.util.stream.Collectors.toMap;
+
+public class Reducer {
+ private Hashtable<String, Integer> map = new Hashtable<String, Integer>();
+ private Map<String, Integer> sorted;
+
+ public Hashtable<String, Integer> getMap(){
+     return map;
+ }
+ 
+ public void setMap(Hashtable<String, Integer> map){
+     this.map=map;
+ }
+ 
+ public void sort(){     
+     sorted = map
+        .entrySet()
+        .stream()
+        .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+        .collect(
+            toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2,
+                LinkedHashMap::new));   
+ }
+ 
+ public void save(){ 
+    try {
+         FileWriter f = new FileWriter("C:\\Users\\kimji\\Desktop\\big data\\3rd assignment\\result.txt");
+         for (Entry<String, Integer> entry : sorted.entrySet()) {
+             f.write(entry.getKey() + ": " + entry.getValue() + "\n");
+         }
+         f.close();
+         System.out.println("Successfully wrote to the file.");
+       } catch (IOException e) {
+         System.out.println("An error occurred.");
+         e.printStackTrace();
+       }
+
+}
+ 
+ public void reduce(ArrayList<String> words){
+     
+     for (int i=0; i<words.size(); i++) {
+         String word = words.get(i);
+         if (map.containsKey(word)) { //check if map hashtable contains the word
+             int count = map.get(word); //get the count in that word
+             count++;
+             map.put(word, count);
+         } else {
+             map.put(word, 1);
+         }
+     }     
+}
+ 
+ public static void main(String[] args) {
+     
+     Mapper m = new Mapper();
+     m.load();
+     Reducer r = new Reducer();
+     ArrayList<String> a = m.getList();
+     r.reduce(a);
+     r.sort();
+     r.save();
+ }
+}
